dd0a1e0ec73257e8336ac43106ce9929ec8583bd
Elysia Moore <username@users.noreply.github.com>
2020-07-17 16:56:32 +0100

    edit bootstrap

--- AstonBookStore/resources/views/welcome.blade.php
+++ AstonBookStore/resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{--<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -9,9 +9,16 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
+       --}}{{-- <!-- Styles -->
         <style>
-            html, body {
+            body {
+                background: url('https://source.unsplash.com/uu0cOMPdM2g/1920x1080') no-repeat center center fixed;
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                background-size: cover;
+                -o-background-size: cover;
+            }
+            /*html, body {
                 background-color: #fff;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
@@ -40,61 +47,30 @@
                 top: 18px;
             }
 
-            .content {
-                text-align: center;
-            }
 
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
 
             .m-b-md {
                 margin-bottom: 30px;
-            }
-        </style>
+            }*/
+
+        </style>--}}{{--
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+    <body>--}}
+@extends('layouts.app')
+@section('content')
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Book Store
-                </div>
+        <div class="row justify-content-center">
+            <div class="flex-center position-ref full-height">
+                <div class="jumbotron align-items-center text-center">
+                    <h1 class="display-4 ">Welcome to Aston Book Store! </h1>
+                    <p class="lead">Browse our books. Register or Login to buy them!</p>
+                    <hr class="my-4">
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a class="btn btn-primary btn-lg" href="{{ url('books') }}" role="button">To The Books!</a>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+
+@endsection
