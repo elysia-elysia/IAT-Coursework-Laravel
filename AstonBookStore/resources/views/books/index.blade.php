13d4409255d0ffbb5a1609d6fccf93c836451141
Elysia Moore <username@users.noreply.github.com>
2020-07-31 15:03:56 +0100

    test

--- AstonBookStore/resources/views/books/index.blade.php
+++ AstonBookStore/resources/views/books/index.blade.php
@@ -126,13 +126,14 @@
                 </div>
             </div>
         </div>
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4-4.1.1/jq-3.3.1/dt-1.10.21/datatables.min.css"/>
-
-<script type="text/javascript" src="https://cdn.datatables.net/v/bs4-4.1.1/jq-3.3.1/dt-1.10.21/datatables.min.js"></script>
-<script>
-    $(document).ready(function() {
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js"> $(document).ready(function() {
         $('#books').DataTable();
-    } );
+    } );</script>
+
+<script>
+
+    alert( 'Hello, world!' );
 </script>
 {{--<script type="text/javascript">--}}
 {{--    $(function () {--}}
--- AstonBookStore/resources/views/layouts/app.blade.php
+++ AstonBookStore/resources/views/layouts/app.blade.php
@@ -18,12 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-{{--    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />--}}
-{{--    <link href="https://cdn.datatables.net/1.10.17/css/jquery.dataTables.min.css" rel="stylesheet">--}}
-{{--    <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">--}}
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4-4.1.1/jq-3.3.1/dt-1.10.21/datatables.min.css"/>
 
-    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4-4.1.1/jq-3.3.1/dt-1.10.21/datatables.min.js"></script>
 </head>
 <body>
     <div id="app">
