cac13c4459dd2554a3d1d0b2b18ab952a2f10ce2
Elysia Moore <username@users.noreply.github.com>
2020-07-31 15:04:57 +0100

    Update index.blade.php

--- AstonBookStore/resources/views/books/index.blade.php
+++ AstonBookStore/resources/views/books/index.blade.php
@@ -127,13 +127,14 @@
             </div>
         </div>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js"> $(document).ready(function() {
-        $('#books').DataTable();
-    } );</script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js"></script>
 
 <script>
+    $(document).ready(function() {
+        $('#books').DataTable();
+        alert( 'Hello, world!' );
+    } );
 
-    alert( 'Hello, world!' );
 </script>
 {{--<script type="text/javascript">--}}
 {{--    $(function () {--}}
