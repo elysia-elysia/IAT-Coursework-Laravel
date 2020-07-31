82e171e14be845fd9210469b284667f285dcb7b4
Elysia Moore <username@users.noreply.github.com>
2020-07-31 15:14:10 +0100

    test

--- AstonBookStore/resources/views/books/index.blade.php
+++ AstonBookStore/resources/views/books/index.blade.php
@@ -126,7 +126,16 @@
                 </div>
             </div>
         </div>
+<link rel="stylesheet" type="text/css" href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.0/css/jquery.dataTables.css">
+<link rel="stylesheet" type="text/css" href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.0/css/jquery.dataTables_themeroller.css">
+<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.1.min.js"></script>
+<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.0/jquery.dataTables.min.js"></script>
+<script>
 
+        $('#books').dataTable();
+
+
+</script>
 {{--<script type="text/javascript">--}}
 {{--    $(function () {--}}
 
