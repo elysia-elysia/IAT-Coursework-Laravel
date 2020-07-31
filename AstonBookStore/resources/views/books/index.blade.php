9ef8323aced0ac133b319695b24f95dead12f6d5
Elysia Moore <username@users.noreply.github.com>
2020-07-31 13:51:11 +0100

    Update index.blade.php

--- AstonBookStore/resources/views/books/index.blade.php
+++ AstonBookStore/resources/views/books/index.blade.php
@@ -108,6 +108,12 @@
                             </tbody>
                         </table>
                     </div>
+                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+                    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+                    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+                    <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+
                     <script>
                         $(function() {
                             $('#books').DataTable({
