e874276e3ebad62d6144545dfcb82040585d0a05
Elysia Moore <username@users.noreply.github.com>
2020-07-31 15:18:23 +0100

    Update index.blade.php

--- AstonBookStore/resources/views/books/index.blade.php
+++ AstonBookStore/resources/views/books/index.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.app')
 @section('content')
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js"></script>
-
+    <script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.1.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.0/jquery.dataTables.min.js"></script>
     <script>
         $(document).ready(function() {
-            $('#books').dataTable();
+            $('#books').DataTable();
         } );
     </script>
 {{--    <div class="container">--}}
