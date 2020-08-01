46a09443a6a33060c035e149dfb2dd66a2de3ec5
Elysia Moore <username@users.noreply.github.com>
2020-08-01 16:34:49 +0100

    Update web.php

--- AstonBookStore/routes/web.php
+++ AstonBookStore/routes/web.php
@@ -17,7 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('books', 'BookController');
-Route::post('books', 'BookController@filterSort');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
