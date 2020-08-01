4777c3061270d0d191c195970a7aa71b3a88c8eb
Elysia Moore <username@users.noreply.github.com>
2020-08-01 14:57:44 +0100

    Update 2020_08_01_145956_create_book_images_table.php

--- AstonBookStore/database/migrations/2020_08_01_145956_create_book_images_table.php
+++ AstonBookStore/database/migrations/2020_08_01_145956_create_book_images_table.php
@@ -14,7 +14,8 @@ class CreateBookImagesTable extends Migration
     public function up()
     {
         Schema::create('book_images', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
+            $table->timestamps();
             $table->integer('book_id')->unsigned();
             $table->foreign('book_id')->references('id')->on('books');
             $table->string('filename');
